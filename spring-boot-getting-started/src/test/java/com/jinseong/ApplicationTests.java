08d306c276c4e1c8516c49b5d35717164371b61d
jinseong <57009926+jinseong205@users.noreply.github.com>
2023-03-30 15:04:33 +0900

    ^^7

---
+++ spring-boot-getting-started/src/test/java/com/jinseong/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinseong;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
